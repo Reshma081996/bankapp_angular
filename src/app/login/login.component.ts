515d2faa3f21d5ede2842a2570e27340ee7dcec7
reshma r <reshma081996@gmail.com>
2021-05-31 22:57:47 +0530

    one way binding - interpolation,property binding ,event binding,event  binding using dollar

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  aim ="Your perfect banking partner"
+  account ="Account Number Please"
+  acno ="" //component class can be fetched using this keyword 
+  pwd=""
+  userdata:any = {
+    1000: { accno: 1000, password: "userone", balance: 5000 },
+    1001: { accno: 1001, password: "usertwo", balance: 5000 },
+    1002: { accno: 1002, password: "userthree", balance: 5000 }
+}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  accChange(event:any){
+    //console.log(event.target.value)
+    this.acno = event.target.value
+    console.log(this.acno);
+  }
+  pwdChange(event:any){
+    this.pwd = event.target.value
+    console.log(this.pwd);
+  }
+ login(){
+    var accno = this.acno;
+    var pswd = this.pwd ;
+    let dataset = this.userdata;
+    if (accno in dataset) {
+      if (pswd == dataset[accno]["password"]){
+        alert("Succcessfully logged in")}
+      else{
+        alert("Incorrect password")}
+   
+    }
+    else{
+      alert("Invalid password")}  
+  }
+}
+
+
+// #application view and component sync ai nikan vendii using databinding method
+// data binding -1way binding(),2way binding   
+
+
+
+
+
+
+
+
+
+
+
